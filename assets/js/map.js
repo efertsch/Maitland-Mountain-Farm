00d1533d1959a96160bd76c3e6716732647b9210
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2017-11-19 16:59:36 -0500

    temporarily removes footer for debugging and adds google places functionality to map

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,14 +1,14 @@
 var locations = [
-  { name: "The Lobster Shanty", latitude: 42.520396, longitude: -70.894639, type: "service"},
-  { name: "Sea Level", latitude: 42.520262, longitude: -70.887796, type: "service"},
-  { name: "The Blue Ox", latitude: 42.463266, longitude: -70.947593, type: "service"},
-  { name: "The Corner Butcher Shop", latitude: 42.556560, longitude: -70.889357, type: "retail"}
+  { name: "The Lobster Shanty", latitude: 42.520396, longitude: -70.894639, type: "service", placeID: "ChIJmxCS2mQU44kR1d30Ke4dkiA"}
+  // { name: "Sea Level", latitude: 42.520262, longitude: -70.887796, type: "service"},
+  // { name: "The Blue Ox", latitude: 42.463266, longitude: -70.947593, type: "service", placeID: },
+  // { name: "The Corner Butcher Shop", latitude: 42.556560, longitude: -70.889357, type: "retail"}
 ]
 
 
 function initMap() {
   var salem = {lat: 42.519539, lng: -70.896713};
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 12,
     center: salem
   });
@@ -21,18 +21,37 @@ function initMap() {
       icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
     }
   };
+
+  var infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
   
   locations.forEach(function(location){
     var coords = {};
-      coords["lat"] = location.latitude
-      coords["lng"] = location.longitude
-    var marker = new google.maps.Marker({
-      icon: icons[location.type].icon,
-      position: coords,
-      map: map
+      coords["lat"] = location.latitude; //dot notation for coords
+      coords["lng"] = location.longitude; //dot notation for coords
+
+    service.getDetails({
+      placeId: location.placeID
+      }, function(place, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        console.log(place);
+        var marker = new google.maps.Marker({
+          icon: icons[location.type].icon,
+          position: coords,
+          // positon: place.geometry.location,
+          map: map
+         });
+
+        google.maps.event.addListener(marker, "click", function() {
+          infowindow.setContent("<div><strong>" + place.name + "</strong><br>" +
+            place.formatted_address + "</div>");
+          infowindow.open(map, this);
+        });
+      }
     });
   });
 }
+
 
 
 
