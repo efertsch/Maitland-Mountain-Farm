39869c7656b905edce5b6a3379cdd024f2ad9212
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2018-03-07 20:22:35 -0500

    adds slideshow to about page

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -10,3 +10,15 @@ $("#slideshow").vegas({
     ],
     animation: 'kenburnsUp'
 });
+
+$("#aboutSlideshow").vegas({
+    timer: false,
+    transitionDuration: 3000,
+    slides: [
+        { src: "assets/images/family/family_window.jpg" },
+        { src: "assets/images/family/holly_in_gh.jpg" },
+        { src: "assets/images/family/jett_with_chickens.jpg" },
+        { src: "assets/images/family/maitland_family.jpg" }
+    ],
+    animation: 'kenburnsUp'
+});
